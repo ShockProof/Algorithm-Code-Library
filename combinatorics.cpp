d438a3092d4970eac82c7effe4ca0c7b4f719665
ShockProof <antistudy9@gmail.com>
2016-08-26 01:36:51 +0600

    nCr

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int LL;
+typedef unsigned long long int uLL;
 inline int _Int() { int x; scanf("%d",&x); return x; }
 LL bigMod(LL A,LL P,int M){ LL R=1; for(A%=M; P; P>>=1) { if(P&1) R=(R*A)%M; A=(A*A)%M; } return R; } /** (A^P) % M **/
 #define pb   push_back
@@ -9,16 +10,29 @@ const int MOD = 1000000007;
 const int M = 2000007;
 
 struct Combinatorics{
+    uLL nCr[31][31];
+
     LL fac[M];
     void preCalculations() {
+
+        for(int i=0; i<31; i++) {
+            for(int j=0; j<31; j++) {
+                if( j==0 ) nCr[i][j] = 1;
+                else if( i==0 ) nCr[i][j] = (j==0);
+                else nCr[i][j] = nCr[i-1][j] + nCr[i-1][j-1];
+            }
+        }
+
         fac[0] = 1;
-        for(int i=1; i<M; i++)
+        for(int i=1; i<M; i++) {
             fac[i] = (fac[i-1]*i)%MOD;
+        }
     }
+
     int invFac(int n) {
         return bigMod( fac[n], MOD-2, MOD );
     }
-    int nCr(int n, int r) {
+    int nCr_mod(int n, int r) {
         return ( ((fac[n] * invFac(n-r))%MOD) * invFac(r) )%MOD;
     }
 }_math;
@@ -27,7 +41,7 @@ void Main()
 {
     int n=_Int();
     int r=_Int();
-    printf("%d\n", _math.nCr(n+r-1,r-1) );
+    printf("%d\n", _math.nCr_mod(n+r-1,r-1) );
 }
 
 int main()
