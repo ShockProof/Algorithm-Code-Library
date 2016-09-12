a0f8e09965e8849fe8a4b24a69654ec638ac0357
ShockProof <antistudy9@gmail.com>
2016-09-13 02:12:47 +0600

    Combibatorics

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -4,6 +4,7 @@ typedef long long int LL;
 typedef unsigned long long int uLL;
 inline int _Int() { int x; scanf("%d",&x); return x; }
 LL bigMod(LL A,LL P,int M){ LL R=1; for(A%=M; P; P>>=1) { if(P&1) R=(R*A)%M; A=(A*A)%M; } return R; } /** (A^P) % M **/
+LL negMod(LL A,LL B) { return ( ( ( A % B ) + B) % B ); } /** (A % B) when A is negative or positive **/
 #define pb   push_back
 
 const int MOD = 1000000007;
@@ -13,8 +14,11 @@ struct Combinatorics{
     uLL nCr[31][31];
 
     LL fac[M];
-    void preCalculations() {
 
+    LL derangement[M];
+
+    void preCalculations()
+    {
         for(int i=0; i<31; i++) {
             for(int j=0; j<31; j++) {
                 if( j==0 ) nCr[i][j] = 1;
@@ -27,6 +31,13 @@ struct Combinatorics{
         for(int i=1; i<M; i++) {
             fac[i] = (fac[i-1]*i)%MOD;
         }
+
+        derangement[0] = derangement[2] = 1;
+        derangement[1] = 0;
+        for(int i=3; i<M; i++) {
+            derangement[i] = (i-1)*(derangement[i-1]+derangement[i-2]);
+            derangement[i] %= MOD;
+        }
     }
 
     int invFac(int n) {
@@ -36,20 +47,22 @@ struct Combinatorics{
         return ( ((fac[n] * invFac(n-r))%MOD) * invFac(r) )%MOD;
     }
     int nth_Catalan_Number(int i) {
+        /** (i>=0) 0, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, ... */
         return negMod(nCr_mod(i+i,i)-nCr_mod(i+i,i+1), MOD);
     }
-}_math;
+}obj;
 
 void Main()
 {
     int n=_Int();
     int r=_Int();
-    printf("%d\n", _math.nCr_mod(n+r-1,r-1) );
+    printf("%d\n", obj.nCr_mod(n+r-1,r-1) );
 }
 
 int main()
 {
-    _math.preCalculations();
+    obj.preCalculations();
+
     int test=_Int();
     while( test-- ) {
         Main();
