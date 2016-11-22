4a22f150c732d4cfe7f36fc89c27420881364ec5
ShockProof <zafrullipu@gmail.com>
2016-11-22 22:44:25 +0600

    Prime Factorization, Segmented Sieve, Divisor function

--- Number Theory/bitwise sieve and NT template.cpp
+++ Number Theory/bitwise sieve and NT template.cpp
@@ -5,37 +5,23 @@ typedef unsigned long long int uLL;
 inline unsigned int _Int() { int x; scanf("%d",&x); return x; }
 
 const int RNG = 1000001;
-int Prime[100000], Prime_sz;
+int Prime[RNG], Prime_sz;
 struct BitwiseSieve
 {
     int is[ (RNG/32)+3 ];
-    bool notPrime(int i) {
-        int x = i>>5;
-        int bit = i-(x<<5);
-        return ( is[x] & (1<<bit) );
-    }
-    void mark(int i) {
-        int x = i>>5;
-        int bit = i-(x<<5);
-        is[x] = is[x]|(1<<bit);
-    }
-    void unmark(int i) {
-        int x = i>>5;
-        int bit = i-(x<<5);
-        is[x] = is[x]^(1<<bit);
-    }
+    bool isMarked(int i) { int x = i>>5; int bit = i-(x<<5); return ( is[x] & (1<<bit) ); }
+    void mark(int i)     { int x = i>>5; int bit = i-(x<<5); is[x] = is[x] | (1<<bit); }
     void precalculations() {
         int MX = sqrt(RNG)+1;
         for(int i=2; i<=MX; i++) {
-            if( !notPrime(i) ) {
+            if( !isMarked(i) ) {
                 for(int j=i+i; j<=RNG; j+=i)
                     mark(j);
             }
         }
-
         Prime_sz = 0;
         for(int i=2; i<RNG; i++) {
-            if( !notPrime(i) ) {
+            if( !isMarked(i) ) {
                 Prime[Prime_sz++] = i;
             }
         }
@@ -86,32 +72,23 @@ struct Segmented_Sieve {
 int divisor(LL x) {
     int Ans = 1;
     int sqrtX = sqrt(x);
-    for(int i=0; Prime[i]<=sqrtX && x>1 && i<Prime_sz; i++) {
+    for(int i=0; i<Prime_sz && Prime[i]<=sqrtX && x>1; i++) {
         int cnt = 0;
         while( (x%Prime[i])==0 ) {
             x/=Prime[i];
             cnt++;
         }
-        if(cnt) Ans=Ans*(cnt+1);
+        if( cnt ) {
+            Ans=Ans*(cnt+1);
+            sqrtX = sqrt(x);
+        }
     }
     if( x>1 ) Ans<<=1;
     return Ans;
 }
 
-
-int Case = 0;
-
-void Main()
-{
-}
-
 int main()
 {
     bitsieve.precalculations();
-    int t=_Int();
-    while(t--) {
-        Case++;
-        Main();
-    }
     return 0;
 }
