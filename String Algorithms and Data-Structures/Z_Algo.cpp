d214ebb80c4ccfb959a4ca50ed805f68ef412bbe
ShockProof <zafrullipu@gmail.com>
2017-01-06 01:45:46 +0600

    Z Algorithm

--- String Algorithms and Data-Structures/Z_Algo.cpp
+++ String Algorithms and Data-Structures/Z_Algo.cpp
@@ -2,26 +2,27 @@
 using namespace std;
 
 void Z_Algorithm( char *s, int n, int z[] ) {
-    int L = 0, R = 0;
-    for(int i=1; i<n; i++) {
-        if( i>R ) {
-            L=R=i;
-            while( R<n && s[R-L] == s[R] ) R++;
-            z[i] = R-L;
-            R--;
-        } else {
-            int k = i-L;
-            if( z[k] < R-i+1) z[i] = z[k];
-            else {
-                L = i;
-                while ( R<n && s[R-L]==s[R] ) R++;
-                z[i] = R-L;
-                R--;
-            }
+        int L = 0, R = 0;
+        for(int i = 1; i < n; i ++ ) {
+                if( i > R ) {
+                        L = R = i;
+                        while( R < n && s[ R-L ] == s[ R ] ) R ++;
+                        z[ i ] = R - L;
+                        R --;
+                }
+                else {
+                        int k = i - L;
+                        if( z[ k ] < R-i+1 ) z[i] = z[k];
+                        else {
+                                L = i;
+                                while ( R < n && s[ R-L ]==s[ R ] ) R++;
+                                z[ i ] = R - L;
+                                R --;
+                        }
+                }
         }
-    }
 }
 
 int main() {
-    return 0;
+        return 0;
 }
