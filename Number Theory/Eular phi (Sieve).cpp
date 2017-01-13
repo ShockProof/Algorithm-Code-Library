92fd9c3d2aed4684ff67d9197fae4855ed7d764c
Syed Zafrul Bahar Lipu <zafrullipu@gmail.com>
2017-01-13 12:30:33 +0600

    Eular phi

--- Number Theory/Eular phi (Sieve).cpp
+++ Number Theory/Eular phi (Sieve).cpp
@@ -1,32 +1,21 @@
 #include<stdio.h>
-
 #define M 5000001
 unsigned long long int phi[M];
-void Precalculation()
+void precal()
 {
-    int i,j;
-    phi[1]=1;
-    for(i=2; i<M; i++)
-    {
-        if(phi[i]==0)
-        {
-            phi[i]=i-1;
-            for(j=i+i; j<M; j+=i)
-            {
-                if(phi[j]==0) phi[j]=j;
-                phi[j]=(phi[j]*(i-1))/i;
-            }
+        int i,j;
+        phi[1] = 1;
+        for( i = 2; i < M; i ++ ) {
+                if( phi[i] != 0 ) continue;
+                phi[i] = i-1;
+                for( j = i + i ; j < M; j += i ) {
+                        if( phi[j] == 0 ) phi[j] = j;
+                        phi[j] = ( phi[j] * (i-1) ) / i;
+                }
         }
-    }
 }
 int main()
 {
-    Precalculation();
-
-    int test;
-    scanf("%d",&test);
-    while( test -- ) {
-        ///
-    }
-    return 0;
+        precal();
+        return 0;
 }
