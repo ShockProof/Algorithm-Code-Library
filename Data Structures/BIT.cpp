3d6d7df5f238ae2ac9b26aa652b7baf1e19d3ae5
ShockProof <zafrullipu@gmail.com>
2017-01-20 21:01:07 +0600

    Binary Indexed Tree

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -7,31 +7,30 @@ const int M = 1000000 + 7;
 
 LL Tree[M];
 int sz = M-5;
+
 LL qry(int i) {
-    LL R = 0;
-    while( i > 0 ) {
-        R += Tree[i];
-        i -= i & (-i);
-    }
-    return R;
+        LL R = 0;
+        while( i > 0 ) {
+                R += Tree[i];
+                i -= i & (-i);
+        }
+        return R;
 }
-LL query(int i, int j) { return qry(j)-qry(i-1); }
+
+LL query(int i, int j) {
+        return qry( j ) - qry( i - 1 );
+}
+
 void update(int i, LL v) {
-    while( i<=sz ) {
-        Tree[i]+=v;
-        i += i & (-i);
-    }
-}
-int clearIndex(int i) {
-    int v = query(i,i);
-    update( i , (-v) );
-    return v;
+        while( i<=sz ) {
+                Tree[i]+=v;
+                i += i & (-i);
+        }
 }
 
-
-
-
-
+void clearIndex(int i) {
+        update( i , -query(i,i) );
+}
 
 int main()
 {
