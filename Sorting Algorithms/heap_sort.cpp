b44312eeaf71c20eed94c089075e8cdd184747b1
Syed Zafrul Bahar Lipu <zafrullipu@gmail.com>
2017-05-02 01:41:53 +0600

    Binary Heap (min)

--- Sorting Algorithms/heap_sort.cpp
+++ Sorting Algorithms/heap_sort.cpp
@@ -6,14 +6,14 @@ const int M = 2000000+7;
 struct BinaryHeap_MIN {
 
         int tree[M];
-        int nnn; /// Tree Size
+        int szz; /// Tree Size
 
         int parent( int x ) { return (x-1)>>1; }
         int chield_1( int x ){return (x<<1)+1; }
         int chield_2( int x ){return (x<<1)+2; }
 
         void insert( int x )  {
-                int pos = nnn++ , prev;
+                int pos = szz++ , prev;
                 tree[pos] = x;
                 prev = parent(pos);
                 while( pos>0 && tree[pos]<tree[prev] ) {
@@ -24,15 +24,15 @@ struct BinaryHeap_MIN {
         }
 
         void remove() {
-                tree[0] = tree[--nnn];
+                tree[0] = tree[--szz];
                 int i = 0 , c1,c2;
                 while( 1 ) {
                         c1 = chield_1(i) , c2 = chield_2(i);
-                        if     ( c1 < nnn && tree[c1] < tree[i] && ( c2 >= nnn || tree[c1]<=tree[c2] ) ) {
+                        if( c1 < szz && tree[c1] < tree[i] && ( c2 >= szz || !(tree[c2]<tree[c1]) ) ) {
                                 swap( tree[i] , tree[c1] );
                                 i = c1;
                         }
-                        else if( c2 < nnn && tree[c2] < tree[i] && ( c1 >= nnn || tree[c2]<=tree[c1] ) ) {
+                        else if( c2 < szz && tree[c2] < tree[i] ) {
                                 swap( tree[i] , tree[c2] );
                                 i = c2;
                         }
@@ -47,7 +47,7 @@ struct BinaryHeap_MIN {
         }
 
         void init() {
-                nnn = 0;
+                szz = 0;
         }
 };
 
