21294893cff45fe51950e2251da90a7508d990a0
Syed Zafrul Bahar Lipu <zafrullipu@gmail.com>
2017-05-10 01:42:01 +0600

    Lazy Propagetion

--- Data Structures/segment tree lazy prop.cpp
+++ Data Structures/segment tree lazy prop.cpp
@@ -2,69 +2,88 @@
 using namespace std;
 typedef long long int LL;
 inline int _Int() { int x; scanf("%d",&x); return x; }
-#define myMemset(a,b) memset(a,b,sizeof(a))
-
 int Case;
+
 const int M = 100000 + 7;
 
-struct lazy_tree_node_data {
-        LL sum;
-        LL lazy;
-}lazy_tree[M<<2];
+LL tree[ M<<2 ];
 
-void lazy_tree_update( int node, int u , int v , int L , int R , LL add , LL carry )
+bool has_lazy[ M<<2 ];
+LL lazy[ M<<2 ];
+
+inline void lazy_pass_down( int node , int c1 , int c2 , int u , int mid , int v )
 {
-        if( v<L || R<u ) {
-                lazy_tree[ node ].lazy += carry;
-                lazy_tree[ node ].sum += carry*(v-u+1);
-                return;
-        }
-        if( L <= u && v <= R ) {
-                lazy_tree[ node ].lazy += add+carry;
-                lazy_tree[ node ].sum += (carry+add)*(v-u+1);
-                return;
-        }
-        int mid = (u+v)>>1; int c1 = (node<<1)+1; int c2 = c1+1;
-        carry+=lazy_tree[node].lazy;
-        lazy_tree_update( c1 , u , mid , L,R,add , carry );
-        lazy_tree_update( c2 , mid+1, v, L,R,add , carry );
-        lazy_tree[node].sum = lazy_tree[c1].sum + lazy_tree[c2].sum;
-        lazy_tree[node].lazy = 0;
+        lazy[c1] += lazy[node];
+        lazy[c2] += lazy[node];
+        has_lazy[c1] = has_lazy[c2] = 1;
+        tree[c1] += lazy[node] * (mid-u+1);
+        tree[c2] += lazy[node] * (v-mid);
+
+        lazy[node] = has_lazy[node] = 0;
+        tree[node] = tree[c1] + tree[c2];
 }
 
-LL lazy_tree_query( int node, int u , int v , int L , int R , LL lazy_carry )
+void lazy_update( int node , int u , int v , int F , int T , int add )
 {
-        if( v<L || R<u ) return 0;
-        if( L<=u && v<=R ) return lazy_tree[node].sum + lazy_carry*(v-u+1);
-        int mid = (u+v)>>1; int c1 = (node<<1)+1; int c2 = c1+1;
-        lazy_carry += lazy_tree[node].lazy;
-        return  lazy_tree_query( c1 , u , mid , L,R , lazy_carry ) +
-                lazy_tree_query( c2 , mid+1,v , L,R , lazy_carry );
+        if( T < u || v < F ) return;
+        if( F<=u && v<=T ) {
+                has_lazy[node] = 1;
+                lazy[node] += add;
+                tree[node] += (LL)add * (v-u+1);
+                return;
+        }
+        int mid = (u+v)>>1 , c1 = node<<1; int c2 = c1+1;
+
+        if( has_lazy[node] )
+                lazy_pass_down( node , c1,c2 , u,mid,v );
+
+        lazy_update( c1 , u , mid , F,T , add );
+        lazy_update( c2 , mid+1,v , F,T , add );
+
+        tree[node] = tree[c1] + tree[c2];
+}
+
+LL lazy_query( int node , int u , int v , int F , int T )
+{
+        if( T < u || v < F ) return 0;
+        if( F<=u && v<=T ) return tree[node];
+        int mid = (u+v)>>1 , c1 = node<<1; int c2 = c1+1;
+
+        if( has_lazy[node] )
+                lazy_pass_down( node , c1,c2 , u,mid,v );
+        return lazy_query( c1 , u,mid , F,T )+lazy_query( c2 , mid+1,v, F,T );
+}
+
+void lazy_init()
+{
+        memset( tree, 0 , sizeof tree );
+        memset( has_lazy , 0 , sizeof has_lazy );
+        memset( lazy , 0 , sizeof lazy );
 }
 
 int n;
 LL rng_sum( int x , int y ) {
-        return lazy_tree_query( 0,0,n , x,y,0 );
+        return lazy_query( 1,0,n , x,y );
 }
 void rng_update( int x , int y , LL add ) {
-        return lazy_tree_update( 0,0,n, x,y,add,0 );
+        return lazy_update( 1,0,n, x,y,add );
 }
 
 void Main()
 {
+        lazy_init();
         n = _Int();
-        memset( lazy_tree , 0 , sizeof lazy_tree );
-
-        int q = _Int() , qtype , x , y , v ;
-        printf("Case %d:\n", Case );
-        while ( q -- ) {
-                qtype = _Int();
-                x = _Int();
-                y = _Int();
-                if( qtype == 1 ) printf("%lld\n", rng_sum( x,y ) );
+        int q = _Int() , qtt , u ,v , add ;
+        printf("Case %d:\n",Case);
+        while( q -- ) {
+                qtt = _Int();
+                u = _Int();
+                v = _Int();
+                if( !qtt ) {
+                        rng_update( u,v, _Int() );
+                }
                 else {
-                        v = _Int();
-                        rng_update( x,y,v );
+                        printf("%lld\n", rng_sum( u,v ) );
                 }
         }
 }
@@ -72,6 +91,7 @@ void Main()
 int main()
 {
         int test = _Int();
-        for( Case = 1 ; Case <= test ; Case ++ ) Main();
+        for( Case = 1 ; Case <= test ; Case ++ )
+                Main();
         return 0;
 }
