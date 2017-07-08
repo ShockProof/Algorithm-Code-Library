651d3f51dd0897318961cab4b035f5d6e62d7f3b
ShockProof <zafrullipu@gmail.com>
2017-07-08 19:53:14 +0600

    the needle in a haystack

--- String Algorithms and Data-Structures/KMP (Knuth_Morris_Pratt) .cpp
+++ String Algorithms and Data-Structures/KMP (Knuth_Morris_Pratt) .cpp
@@ -1,52 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 /**
-    tutorial link : (TopCoder)
-    http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=stringSearching
+tutorial link : (TopCoder)
+http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=stringSearching
 */
 
 struct Knuth_Morris_Pratt {
 
-    int  F[1000009]; /// failure function; starts from 1 to len (pattern length)
-    char text[1000009];                /// starts from 0 to len-1
-    char pattern[1000009];             /// starts from 0 to len-1
-    int n,m; /// n=test_length and m=pattern_length
+        int  F[1000009]; /// failure function; starts from 1 to len (pattern length)
+        char text[1000009];                /// starts from 0 to len-1
+        char pattern[1000009];             /// starts from 0 to len-1
+        int n,m; /// n=test_length and m=pattern_length
 
-    void build_failure_function()
-    {
-        F[0] = F[1] = 0;
-        for(int j, i = 2; i <= m; i++ ) {
-            j = F[i - 1];
-            while( j && pattern[j]!=pattern[i-1]) j=F[j];
-            F[i] = j + (pattern[j]==pattern[i-1]);
-        }
-    }
-
-    int indx[1000009], sz;
-    /// this array contains the starting positions of matches
-
-    int kmp_match()
-    {
-        n=strlen(text), m=strlen(pattern);
-        build_failure_function();
-        for(int i=sz=0, j=0; j<n; j++)
+        void build_failure_function()
         {
-            while( i && text[j]!=pattern[i] ) i = F[i];
-            if( text[j]==pattern[i] ) {
-                i++;
-                if(i == m) {
-                    indx[sz++] = j - m + 1;
-                    i=F[i];
+                F[0] = F[1] = 0;
+                for(int j, i = 2; i <= m; i++ ) {
+                        j = F[i - 1];
+                        while( j && pattern[j]!=pattern[i-1]) j=F[j];
+                        F[i] = j + (pattern[j]==pattern[i-1]);
                 }
-            }
         }
-        return sz;
-    }
+
+        int indx[1000009], sz;
+        /// this array contains the starting positions of matches
+
+        int kmp_match()
+        {
+                n=strlen(text), m=strlen(pattern);
+                build_failure_function();
+                for(int i=sz=0, j=0; j<n; j++)
+                {
+                        while( i && text[j]!=pattern[i] ) i = F[i];
+                        if( text[j]==pattern[i] ) {
+                                i++;
+                                if(i == m) {
+                                        indx[sz++] = j - m + 1;
+                                        i=F[i];
+                                }
+                        }
+                }
+                return sz;
+        }
 } kmp;
 
 int main()
 {
-    scanf("%s %s", kmp.text, kmp.pattern );
-    printf("%d\n", kmp.kmp_match() );
-    return 0;
+        scanf("%s %s", kmp.text, kmp.pattern );
+        printf("%d\n", kmp.kmp_match() );
+        return 0;
 }
--- String Algorithms and Data-Structures/hash.cpp
+++ String Algorithms and Data-Structures/hash.cpp
@@ -9,16 +9,16 @@ char s[1000009];
 
 int main()
 {
-    scanf("%s",s);
-    int len=strlen(s);
-    LL h1=0,h2=0, X=1;
-    int ans=len;
-    for(int i=len-1, po=0; i>0; i--) {
-        h1 = ( ( (h1*B)%Mod ) + (LL)s[i] ) % Mod;
-        h2+= ((LL)s[i]*X) % Mod;
-        X=(X*B)%Mod;
-        h2%=Mod;
-        if(h1==h2) ans=i;
-    }
-    return 0;
+        scanf("%s",s);
+        int len=strlen(s);
+        LL h1=0,h2=0, X=1;
+        int ans=len;
+        for(int i=len-1, po=0; i>0; i--) {
+                h1 = ( ( (h1*B)%Mod ) + (LL)s[i] ) % Mod;
+                h2+= ((LL)s[i]*X) % Mod;
+                X=(X*B)%Mod;
+                h2%=Mod;
+                if(h1==h2) ans=i;
+        }
+        return 0;
 }
