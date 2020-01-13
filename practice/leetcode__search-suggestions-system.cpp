ef3ffbc960083fbe6a28450f9cedbc4e9a1975e9
ShockProof <zafrullipu@gmail.com>
2020-01-13 21:56:19 +0600

    init practice

---
+++ practice/leetcode__search-suggestions-system.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Link:
+ * https://leetcode.com/problems/search-suggestions-system/
+ **/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+  return {};
+}
+
+int main() {
+  vector<string> products = {"mobile","mouse","moneypot","monitor","mousepad"};
+  string searchWord = "mouse";
+
+  vector<vector<string>> output = suggestedProducts(products, searchWord);
+
+  vector<vector<string>> ac_output = {
+    {"mobile","moneypot","monitor"},
+    {"mobile","moneypot","monitor"},
+    {"mouse","mousepad"},
+    {"mouse","mousepad"},
+    {"mouse","mousepad"},
+  };
+
+  cout << ((output == ac_output) ? "Acceppted" : "Wrong Answer") << endl;
+
+  return 0;
+}
